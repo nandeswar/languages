500d5a91387505f64776d184416de1225f2ac800
nandu <nandeswar114>
2019-01-28 04:57:54 +0000

    CLanguage

--- JavaScript/Twonumbers.js
+++
@@ -1,9 +0,0 @@
-var Count = parseInt(console.log("How many natural numbers you want to print?"))
-var input = 1;
-var Counter = "";
-while (input <= Count){
-      Counter += " " + input;
-      input++;}
-    console.log("The first "+Count+" natural numbers are "+Counter+".");
-
-
