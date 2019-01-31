3e4d6f86264626a17156983ccfcaedfc6cd88487
nandeswar <nandeswar114@gmail.com>
2019-01-31 13:13:29 +0530

    addition

---
+++ CLanguage/sum.c
@@ -0,0 +1,14 @@
+
+
+#include <stdio.h>
+int main(void) 
+{
+  int NumberOne, NumberTwo, Sum;
+  printf("To add  two numbers, enter the first number:");
+  scanf("%d", &NumberOne);
+  printf("Enter the second number:");
+  scanf("%d", &NumberTwo);
+  Sum = NumberOne + NumberTwo;
+  printf("The sum of %d and %d is %d.", NumberOne, NumberTwo, Sum);
+  return 0;
+}[A[A[A[A[A[A[A[A[A[[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[C
